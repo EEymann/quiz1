00d1c258059c266e531c57ddf404e9d82800fc57
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-09-05 11:00:48 -0700

    Create a multidimensional array to hold quiz questions and answers.

--- js/quiz.js
+++ js/quiz.js
@@ -1,10 +1,16 @@
 // 1. Create a multidimensional array to hold quiz questions and answers
 
+const questions = [
+  ['How many planets are in the Solar System?', '8'],
+  ['How many continents are there?', '7'],
+  ['How many legs does an insect have?', '6'],
+  ['What year was JavaScript created?', '1995']
+];
 
 // 2. Store the number of questions answered correctly
 
 
-/* 
+/*
   3. Use a loop to cycle through each question
       - Present each question to the user
       - Compare the user's response to answer in the array
